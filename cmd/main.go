abeb8b45a15a69676de801f245654443ff112815
patihomirov <patihomirov@gmail.com>
2022-06-02 22:21:23 +0300

    Введено небольшое самотестирование при старте сервиса

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"google.golang.org/grpc"
 	"librarium/api/proto"
 	"librarium/server"
@@ -12,7 +14,9 @@ var ServiceOnline bool //Эта переменная нужна для функ�
 
 func main() {
 	ServiceOnline = true
-	log.Print("Service librarium is started at port 8080")
+	if selftests() {
+		log.Print("Service librarium is started at port 8080")
+	}
 
 	s := grpc.NewServer()
 	srv := &server.Server{}
@@ -26,4 +30,28 @@ func main() {
 	if err := s.Serve(l); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func selftests() bool {
+	log.Println("selftesting is started")
+	log.Println("mySQL access testing")
+
+	s := server.Server{}
+	var req proto.DataRequest
+	//Arange
+	req.AskMessage = "Грэм Грин"
+	expected := `replyMessage:"Сила и слава"`
+
+	//Act
+	result, _ := s.GetBooks(context.Background(), &req)
+
+	//Assert
+	if fmt.Sprint(result) == fmt.Sprint(expected) {
+		log.Println("testing is done")
+		return true
+	} else {
+		log.Fatalf("testing is fail. Expect %s, got %s", expected, result)
+		return false
+	}
+
 }
